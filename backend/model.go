d0e6c0d0a354f220e4c3b8bf7f72c7a206e62579
ppolariss <2673986789@qq.com>
2025-04-10 17:10:56 +0800

    fix: update struct form

--- backend/model.go
+++ backend/model.go
@@ -5,10 +5,10 @@ import (
 )
 
 type ListModel struct {
-	Size    int    `json:"size" query:"size" default:"30" validate:"min=0,max=50"`          // length of object array
-	Offset  int    `json:"offset" query:"offset" default:"0" validate:"min=0"`              // offset of object array
-	Sort    string `json:"sort" query:"sort" default:"asc" validate:"oneof=asc desc"`       // Sort order
-	OrderBy string `json:"order_by" query:"order_by" default:"id" validate:"oneof=id like"` // SQL ORDER BY field
+	Size    int    `json:"size" query:"size" form:"size" default:"30" validate:"min=0,max=50"`          // length of object array
+	Offset  int    `json:"offset" query:"offset" form:"offset" default:"0" validate:"min=0"`              // offset of object array
+	Sort    string `json:"sort" query:"sort" form:"sort" default:"asc" validate:"oneof=asc desc"`       // Sort order
+	OrderBy string `json:"order_by" query:"order_by" form:"order_by" default:"id" validate:"oneof=id like"` // SQL ORDER BY field
 }
 type Floor struct {
 	/// saved fields
